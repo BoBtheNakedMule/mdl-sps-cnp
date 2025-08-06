10113fc1428614edbe885fa96566ee0cee1bf861
BoBtheNakedMule <bobthemule@gmail.com>
2025-08-06 10:29:53 -0400

    Add files via upload

--- _soybean.py
+++ _soybean.py
@@ -1,4 +1,4 @@
-#pyinstaller C:\SPS_python\.venv\Marshall\_nifa-afri.py --onefile
+#pyinstaller C:\SPS_python\.venv\Marshall\_soybean.py --onefile
 import openpyxl
 from docx import Document
 from docx.shared import Inches, Pt
@@ -27,7 +27,7 @@ def open_file():
     file_path = filedialog.askopenfilename(
         filetypes=[("Excel files", "*.xlsx")],
         defaultextension=".docx",
-        title="SPS Current and Pending -NIFA-AFRI",
+        title="SPS Current and Pending -Indiana Soy and Corn",
         initialdir=r"K:\_DeptAll\PreAward\3. Administrative\Faculty Documents - CPs\Faculty Current & Pending"
     )
 
@@ -45,7 +45,7 @@ def save_file(workbook_file_path):
     workbook_file_path = os.path.splitext(workbook_file_path)[0]
     save_path = filedialog.asksaveasfilename(
         filetypes=[("Word files", "*.docx")],
-        title="SPS Current and Pending -NIFA-AFRI",
+        title="SPS Current and Pending -Indiana Soy and Corn",
         initialdir=r"K:\_DeptAll\PreAward\3. Administrative\Faculty Documents - CPs\Faculty Current & Pending",
         initialfile=f"{workbook_file_path}.docx"
     )
@@ -91,7 +91,7 @@ def set_cell_border(cell, **kwargs):
 
 
 # Main code
-version = "SOYBEAN-CORN-20250801"
+version = "SOYBEAN-CORN-20250806"
 print(20 * "*")
 print(f"Version: {version}")
 print("When reporting issues, please provide this version number")
@@ -130,35 +130,8 @@ for row in sheet.iter_rows(min_row=41, values_only=True):
             row[headers.index('Project Title')]
         ))
 
-# Function to create table in Word document
-def create_table(doc, projects, title, category):
-
-    #formats funding amount with dollar sign and two decimals. Handles error if the funding column has text
-    def currency_formatting(funding_column):
-        try:
-            funding_float = float(funding_column if funding_column is not None else '')
-            return f'${funding_float:,.0f}'
-        except ValueError:
-            print(f"The Funding Number is NOT text and says {funding_column}.\n Edit the Word file after saving or fix the Excel file and run this script again.")
-            return '**** Incorrect Entry- Must be in format of ####.## ****'
-    
-    #Error checking to ensure other columns don't have blank spaces in needed rows.    
-    def other_column_blank(data, label):
-        if data is not None:
-            return data
-        else:
-            print(f"Something is blank that shouldn't be, check {label} column")
-            print("No File Saved")
-            os.system(command="pause")
-            exit() 
-            
-    table = doc.add_table(rows=2, cols=6)
-    table.autofit = True
-    table.style = 'Plain Table 4'
-    table.alignment = WD_TABLE_ALIGNMENT.CENTER
-
-    # Set custom borders for all cells
-     
+#function to set table borders        
+def set_table_borders(table):
     n_rows = len(table.rows)
     n_cols = len(table.columns)
     for i, row in enumerate(table.rows):
@@ -199,7 +172,34 @@ def create_table(doc, projects, title, category):
                 set_cell_border(
                     cell, right={"sz": 4, "val": "single", "color": "000000", "space": "0"}
                 )
- 
+
+# Function to create table in Word document
+def create_table(doc, projects, title, category):
+
+    #formats funding amount with dollar sign and two decimals. Handles error if the funding column has text
+    def currency_formatting(funding_column):
+        try:
+            funding_float = float(funding_column if funding_column is not None else '')
+            return f'${funding_float:,.0f}'
+        except ValueError:
+            print(f"The Funding Number is NOT text and says {funding_column}.\n Edit the Word file after saving or fix the Excel file and run this script again.")
+            return '**** Incorrect Entry- Must be in format of ####.## ****'
+    
+    #Error checking to ensure other columns don't have blank spaces in needed rows.    
+    def other_column_blank(data, label):
+        if data is not None:
+            return data
+        else:
+            print(f"Something is blank that shouldn't be, check {label} column")
+            print("No File Saved")
+            os.system(command="pause")
+            exit() 
+            
+    table = doc.add_table(rows=2, cols=6)
+    table.autofit = True
+    table.style = 'Plain Table 4'
+    table.alignment = WD_TABLE_ALIGNMENT.CENTER
+   
     
     # Set header row
     hdr_cells = table.rows[0].cells
@@ -216,47 +216,12 @@ def create_table(doc, projects, title, category):
     category_cells[1].text = f"{category}\n"
     for i in range(2, 6):
         category_cells[i].text = ""
-    
-    # Make the category row bold
-    for cell in table.rows[1].cells:
-        for paragraph in cell.paragraphs:
-            for run in paragraph.runs:
-                run.font.bold = True
-    
+       
     
     # Add project data
     for index, (person, agency, amount, dates, time, title) in enumerate(projects):
         row_cells = table.add_row().cells
-        for j, cell in enumerate(row_cells):
-            # Set custom borders for new rows
-            set_cell_border(
-                cell,
-                left={"sz": 0, "val": "double", "color": "#000000", "space": "0"},
-                top={"sz": 0, "val": "none", "color": "#000000", "space": "0"}
-            )
-            # Add right border for all cells except the last column
-            if j < len(row_cells) - 1:
-                set_cell_border(
-                    cell,
-                    right={"sz": 4, "val": "single", "color": "#000000", "space": "0"}
-                )
-            else:
-                set_cell_border(
-                    cell,
-                    right={"sz": 0, "val": "double", "color": "#000000", "space": "0"}
-                )
-            
-            # Add bottom border only for the last row
-            if index == len(projects) - 1:
-                set_cell_border(
-                    cell,
-                    bottom={"sz": 4, "val": "double", "color": "#000000", "space": "0"}
-                )
-            else:
-                set_cell_border(
-                    cell,
-                    bottom={"sz": 0, "val": "single", "color": "#000000", "space": "0"}
-                )
+
         #Add Data to single row
         row_cells[0].text = str(other_column_blank(person, "PI Name"))
         row_cells[1].text = str(other_column_blank(agency, "NIFA Agency Source"))
@@ -265,7 +230,7 @@ def create_table(doc, projects, title, category):
         row_cells[4].text = str(other_column_blank(time, "Percentage of Time Committed"))
         row_cells[5].text = str(other_column_blank(title, "Title"))
 
-
+    set_table_borders(table)
 
 
     for row in table.rows:
@@ -276,27 +241,6 @@ def create_table(doc, projects, title, category):
                 paragraph.paragraph_format.line_spacing_rule = WD_LINE_SPACING.SINGLE
                 paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
                 #cell.width = Inches(1.12)
-                
-
-'''def afri_parapgraph_check():
-    
-
-    while True:
-        sponsor_type= input("Is this for AFRI (A) or NIFA (N) -Please answer with just one letter: ").lower()
-        if sponsor_type == 'a' :
-            paragraph = doc.add_paragraph()
-            run = paragraph.add_run("As an addendum to the Current and Pending Support, provide a brief summary below for any completed, current, or pending projects that appear similar to the current application, especially previous National Research Initiative (NRI) or AFRI awards: None")
-            run.font.name = "Calibri (Body)"
-            run.font.size = Pt(11)
-            break #exit loop
-             
-        elif sponsor_type == 'n':
-            break #exit loop
-
-        else:
-            print("*" * 20)
-            print("INVALID ENTRY")
-            sponsor_type = input("Please enter A for Afri or N for NIFA ").lower()'''
             
 
 
@@ -335,43 +279,10 @@ def create_document():
     topline.alignment = WD_ALIGN_PARAGRAPH.CENTER
     second_text.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-    # Add a table with 1 row and 1 column
-    header_table = document.add_table(rows=1, cols=1) 
-    header_table.alignment = WD_TABLE_ALIGNMENT.CENTER   
-    header_table.autofit = True
-    '''for column in header_table.columns:
-        for cell in column.cells:
-            cell.width = Inches(6.7) '''
 
-    # Get the cell and add the text
-    cell = header_table.cell(0, 0)
-    paragraph = cell.paragraphs[0]
-    #run = paragraph.add_run("\nCURRENT AND PENDING RESEARCH SUPPORT DISCLOUSRE")
-    #run = paragraph.add_run("\nFY 2026 Reserch Grant Application")
-
-    set_cell_border(
-                    cell,
-                    top={"sz": 18, "val": "none", "color": "#000000", "space": "0"},
-                    bottom={"sz": 18, "val": "none", "color": "#000000", "space": "0"},
-                    left={"sz": 18, "val": "none", "color": "#000000", "space": "0"},
-                    right={"sz": 18, "val": "none", "color": "#000000", "space": "0"}
-                )
-
-    # Center the text
-    paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-
-    # Set the font to Calibri (Body), 12pt
-    font = run.font
-    font.name = 'Calibri (Body)'
-    font.size = Pt(12)
-    font.bold = False
 
     info_table = document.add_table(rows=1, cols=1)
     info_table.alignment = WD_TABLE_ALIGNMENT.CENTER
-
-    # Name row
-    '''pi_name = sheet.cell(4,2).value.split(": ")[1]
-    add_formatted_paragraph(info_table.cell(0, 0), f"Name: {pi_name}", font_size=12, bold=True)'''
 
     # Instructions row
     sec_cell = info_table.cell(0, 0)
@@ -387,20 +298,12 @@ def create_document():
     for point in bullet_points:
         add_formatted_paragraph(sec_cell, point, bullet=True, indent=0.5)
 
+    set_table_borders(info_table)
 
     # Get the second row of the info_table so you can set the border correctly
     #second_row = info_table.rows[0]
 
     # Add top border to each cell in the second row
-
-    '''for cell in second_row.cells:
-        set_cell_border(
-            cell,
-            top={"sz": 4, "val": "none", "color": "#FFFFFF", "space": "0"},
-            left={"sz": 4, "val": "double", "color": "#FFFFFF", "space": "0"},
-            right={"sz": 4, "val": "none", "color": "#FFFFFF", "space": "0"},
-            bottom={"sz": 4, "val": "none", "color": "#FFFFFF", "space": "0"}
-        )'''
 
     return document
 
